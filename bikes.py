ee50c184640bc59153150e4e49a0bfff66495b49
Tom Parker <palfrey@tevp.net>
2010-10-28 21:56:26 +0100

    Factor out non-bikes code into kml.py

--- bikes.py
+++ bikes.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python
 
-from xml.dom.minidom import  parseString
+from xml.dom.minidom import parseString
 import urlgrab
+from kml import kml_string
 
 cache = urlgrab.Cache(cache=".cache")
 data = cache.get("http://borisapi.heroku.com/stations.xml").read()
 
 tree = parseString(data)
-
-print (
-   '<?xml version="1.0" encoding="UTF-8"?>\n'
-   '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
-   '<Document>\n'
-   )
- 
+places = {}
 for station in tree.getElementsByTagName("station"):
-	
-	placemark = (
-	'<Placemark>\n'
-	   '<name>%s</name>\n'
-	   '<Point>\n'
-	   '<coordinates>%f,%f</coordinates>\n'
-	   '</Point>\n'
-	   '</Placemark>\n'
-	) % (station.getElementsByTagName("name")[0].firstChild.data.replace("&","&amp;"),
-		float(station.getElementsByTagName("long")[0].firstChild.data),
-		float(station.getElementsByTagName("lat")[0].firstChild.data)
-		)
-	print placemark
-	#break
+	places[station.getElementsByTagName("name")[0].firstChild.data.replace("&","&amp;")] = \
+		(float(station.getElementsByTagName("long")[0].firstChild.data),
+		float(station.getElementsByTagName("lat")[0].firstChild.data))
 
-print '</Document></kml>'
+print kml_string(places)
---
+++ kml.py
@@ -0,0 +1,18 @@
+def kml_string(places):
+	ret = '<?xml version="1.0" encoding="UTF-8"?>\n' \
+   + '<kml xmlns="http://www.opengis.net/kml/2.2">\n' \
+   + '<Document>\n'
+ 
+	for place in places.keys():
+		(lon, lat) = places[place]
+		placemark = (
+		'<Placemark>\n'
+		   '<name>%s</name>\n'
+		   '<Point>\n'
+		   '<coordinates>%f,%f</coordinates>\n'
+		   '</Point>\n'
+		   '</Placemark>\n'
+		) % (place,lon,lat)
+		ret += placemark
+
+	return ret +'</Document></kml>'
